6746a4281a5ea94439b9aaa891c6bd7c25168eb0
SeuDemar <166663601+SeuDemar@users.noreply.github.com>
2024-06-04 22:22:13 -0300

    Add files via upload

--- Trabalho para meu mano Rodney.c
+++ Trabalho para meu mano Rodney.c
@@ -3,6 +3,7 @@
 #include <windows.h>
 #include <conio.h>
 
+
 void gotoxy(int x, int y)
 {
     COORD coord;
@@ -37,36 +38,93 @@ void tela()
         gotoxy(79, t);
         printf("|");
     }
+    gotoxy(1, 22);
+    printf("+-----------------------------------------------------------------------------+");
+    gotoxy(3, 23);
+    printf("MSG :");
     gotoxy(1, 24);
     printf("+-----------------------------------------------------------------------------+");
     
 }
 
-int main() {
-    tela ();
-    gotoxy();
+void TelaCadastroMovimentacao()
+{
+    tela();
+    printf("**Especificacoes da movimentacao**");
+    gotoxy(03, 5);
     printf("Codigo Do Produto......: ");
-    gotoxy();
+    gotoxy(03, 6);
     printf("Data da Movimentacao...: ");
-    gotoxy();
+    gotoxy(03, 7);
     printf("Data da movimentacao...: ");
-    gotoxy();
+    gotoxy(03, 8);
     printf("Tipo de Movimentacao...: ");
-    gotoxy();
+    gotoxy(03, 9);
     printf("Quantidade.............: ");
-    gotoxy();
+    gotoxy(03, 10);
     printf("Valor Unitario.........: ");
-    gotoxy();
+    gotoxy(03, 11);
     printf("Valor Total............: ");
-    gotoxy();
+    gotoxy(1, 19);
     printf("+-----------------------------------------------------------------------------+");
-    gotoxy();
+    gotoxy(1, 20);
     printf("|       Quantidade       |       Custo Medio       |       Valor Total        |");
-    gotoxy();
+    gotoxy(1, 21);
     printf("+-----------------------------------------------------------------------------+");
-    gotoxy()
+    gotoxy(1, 22);
     printf("|                        |                         |                          |");
-    gotoxy();
+    gotoxy(1, 23);
     printf("+-----------------------------------------------------------------------------+");
+}
+
+void MenuDeCadastroProduto(){
+
+}
+
+void ConsultarProduto() {
+    
+    tela();
+
+    gotoxy(03, 05);
+    printf("1 - Consultar Fichario do Produto Geral");
+    gotoxy(03, 05);
+    printf("2 - Consultar as ordens de codigo");
+    gotoxy(03, 05);
+    printf("3 - Consultar o codigo alfabetica");
+    gotoxy(03, 05);
+    printf("4 - Consultar codigo especifico");
+    gotoxy(03, 05);
+    printf("5 - Retornar menu principal");
+}
+
+int main() {   
+    int opc;
+
+    tela();
+    
+    gotoxy(03, 05);
+    printf("1 - Menu cadastro de produtos");
+    gotoxy(03, 06);
+    printf("2 - Menu movimentacao do estoque ");    
+    gotoxy(03, 07);
+    printf("3 - Finalizar programa");
+    gotoxy(03, 8);
+    printf("Digite uma opcao : ");
+    gotoxy(1, 24);
+    scanf("%d", &opc);
+
+    switch (opc)
+    {
+    case 1 :
+        ConsultarProduto();
+
+        break;
+    
+    default:
+        break;
+    }
+
+    return 0;
+
 }
 
